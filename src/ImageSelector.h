b1215da5770c320d44a60e4f86ac48f23cf350dc
jvbrates <jv.belmonterates@gmail.com>
2024-04-07 17:55:58 -0300

    Se eu esqueci arquivo cpp, é esperado que tenha esquecido o header

---
+++ src/ImageSelector.h
@@ -0,0 +1,86 @@
+//
+// Created by jvbrates on 3/28/24.
+//
+
+#ifndef COMP_GRAFICA_IMAGESELECTOR_H
+#define COMP_GRAFICA_IMAGESELECTOR_H
+
+#include "EventListener.h"
+#include "ImageManagement.h"
+#include "Conteiner.h"
+
+
+class Quadrilatero : public Renderizable{
+public:
+    colors_enum color = yellow;
+    Vector2<float> p1 = {0., 0.},
+            p2 = {0., 0.},
+            p3 = {0., 0.},
+            p4 = {0., 0.};
+
+    void render() override{
+        CV::color(color);
+        CV::polygonFill({{p1,p2},
+                     {p2,p3},
+                     {p3,p4},
+                     {p4,p1}});
+    }
+};
+
+
+
+class ImageSelector;
+
+class dbg: public Renderizable{
+
+public:
+    explicit dbg(ImageSelector *i);
+    ImageSelector *img;
+    void render() override;
+};
+
+class ImageSelector: public EventClient, public Renderizable{
+private:
+
+    friend class dbg;
+    std::function<void(std::shared_ptr<Image>)> f_callback;
+
+
+    uint posImages = -1;
+    bool mouse_left_press = false;
+    bool mouse_right_press = false;
+    Vector2<float> vec_mouse_rigth_click = {0.,0.};
+    float save_rotate;
+
+    std::shared_ptr<Quadrilatero> quadri;
+
+    void swap(int i);
+    void sort();
+    std::shared_ptr<Image> * poiter;
+
+public:
+
+    explicit ImageSelector(std::function<void(std::shared_ptr<Image>)> function);
+    explicit ImageSelector(std::shared_ptr<Image> * pointer);
+    explicit ImageSelector();
+    void setCallback(std::function<void(std::shared_ptr<Image>)> function);
+    void select();
+
+    void loadImage(Image Img, enum_colors channel, Vector2<float> position);
+    void loadImage(std::string path, Vector2<float> position);
+
+    void render() override;
+    bool mouse_left(int state) override;
+    bool mouse_right(int state) override;
+    bool mouse_move(Vector2<float> pos, Vector2<float> desloc) override;
+    bool key_press(int) override;
+    void updateQuadri();
+
+    //Menu Comunication:
+    std::shared_ptr<Image> getSelected();
+
+    Conteiner images;
+};
+
+
+#endif //COMP_GRAFICA_IMAGESELECTOR_H
