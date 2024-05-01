81c55d56ab35f284b7288116459bb22e07c9df8e
João Vitor Belmonte Rates <jvrates@inf.ufsm.br>
2024-05-01 15:46:33 -0300

    Adicionado frame e "alarme"

---
+++ src/Frames.cpp
@@ -0,0 +1,64 @@
+#include "Frames.h"
+
+struct alarm
+{
+    std::shared_ptr<bool> value = std::make_shared<bool>(false);
+    clock_t expire_at;
+
+    alarm(clock_t t): expire_at(t) {}
+
+};
+
+std::vector<alarm_t> Frames::alarms = std::vector<alarm_t>();
+
+//Incialização
+clock_t Frames::t1 = clock();
+clock_t Frames::t2 = Frames::t1;
+long Frames::cont_frames = 0;
+float Frames::fps_old = 20.f;
+float Frames::fps = 0.f;
+
+void Frames::updateAlarms(clock_t time)
+{
+        for(auto i = Frames::alarms.begin(); i != Frames::alarms.end();){
+            if(i->expire_at <= time){
+               *(i->value) = true;
+               i = Frames::alarms.erase(i);
+            } else {
+                break;
+            }
+    }
+}
+
+float Frames::getFrames()
+{
+    double tempo;
+
+    t2 = clock();
+
+    Frames::updateAlarms(t2);
+
+    tempo  = (double)(t2 - t1);  //milisegundos
+    cont_frames++;
+    //apos UPDATE_RATE milisegundos, atualiza o framerate
+    if( tempo > UPDATE_RATE )
+    {
+        t1 = t2;
+        fps = cont_frames / (tempo/CLOCKS_PER_SEC);
+        cont_frames = 0;
+        fps_old = fps;
+
+        return fps;
+    }
+    //se nao foi decorrido UPDATE_RATE milisegundos, retorna a taxa antiga
+    return fps_old;
+}
+
+std::shared_ptr<bool> Frames::makeAlarm(clock_t t)
+{
+    auto new_alarm = alarm_t(clock() + t);
+
+    Frames::alarms.push_back(new_alarm);
+
+    return new_alarm.value;
+};
---
+++ src/Frames.h
@@ -0,0 +1,32 @@
+
+#ifndef _FRAMES_H___
+#define _FRAMES_H___
+
+#include <time.h>
+#include <vector>
+#include <memory>
+#include <iostream>
+
+#define UPDATE_RATE 30   //milisegundos
+#define SECOND 1000   //1 segundo
+
+typedef struct alarm alarm_t;
+
+class Frames
+{
+
+    private:
+        static clock_t t1;
+        static clock_t t2;
+        static long cont_frames;
+        static float   fps, fps_old;
+        static std::vector<alarm_t> alarms ;
+        static void updateAlarms(clock_t t);
+
+    public:
+        static float getFrames();
+        static std::shared_ptr<bool> makeAlarm(clock_t t);
+
+};
+
+#endif
