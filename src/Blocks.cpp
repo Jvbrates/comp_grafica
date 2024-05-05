cef0a4131de08b5fe2d92a8fe9de016e5fa7e6b0
João Vitor Belmonte Rates <jvrates@inf.ufsm.br>
2024-05-05 16:19:12 -0300

    Colisao em andamento, falta 1 função(ou mais)

--- src/Blocks.cpp
+++ src/Blocks.cpp
@@ -18,26 +18,73 @@ Blocks::~Blocks()
     //dtor
 }
 
-typedef struct {
-    Vector2 pos_final;
-    float used;
-} data_moveCircle;
 
-Vector2 Blocks::moveCircle(Vector2 pos, Vector2 movement, float radius){
+data_moveCircle Blocks::moveCircle(Vector2 pos, Vector2 movement, float radius){
+    //Como a grade inteira de blocos pode estar movida, então pos é traduzido para posição correta
+    auto tpos = pos - this->poligonos.getAbsolutePos();
 
-    pos = pos - this->poligonos.getAbsolutePos();
+    data_moveCircle dt = this->recursiveMoveCircle(tpos, movement, radius);
+
+    //Retorna translação
+    dt.pos_final+= this->poligonos.getAbsolutePos();
+
+    return dt;
+
+    }
 
 
-   std::vector<std::shared_ptr<Polygon_>> filtered_blocks = this->selectByRectangle(pos, movement);
+
+void Blocks::sanitizePolygons(){
+        //Remove blocos com life == 0
+    this->poligonos.elements.erase(
+               std::remove_if(this->poligonos.elements.begin(), this->poligonos.elements.end(),
+                [](std::shared_ptr<Renderizable> item) {
+                std::shared_ptr<SpecialSquare> cast_item = std::dynamic_pointer_cast<SpecialSquare>(item);
+                if(cast_item->getLife() == 0) return true;}
+                )
+              , this->poligonos.elements.end()
+              );
 }
 
-std::vector<std::shared_ptr<Polygon_>> Blocks::selectByRectangle(Vector2 pos, Vector2 movement){
+// Será responsavel por calcular a existencia de colisão, caso exista retorna
+// collision: true
+// pos: posicao da colisao
+// reflex: Vetor de reflexão que será usado recursivamente
+data_moveCircle Blocks::firstCollision(std::vector<std::shared_ptr<Polygon_>> filtered, Vector2 pos, Vector2 mov){
+    return data_moveCircle();
+}
+
+data_moveCircle Blocks::recursiveMoveCircle(Vector2 pos, Vector2 movement, float radius){
+
+
+    //Filtra quais blocos possivelmente participaram da colisão
+   std::vector<std::shared_ptr<Polygon_>> filtered_blocks = this->selectByRectangle(pos, movement);
+
+
+   //Caso não haja nenhuma colisão
+   if(filtered_blocks.size() == 0){
+    return data_moveCircle{pos+movement,movement};
+   }
+
+   data_moveCircle dtMove = this->firstCollision(filtered_blocks, pos, movement);
+
+
+   this->sanitizePolygons();
+
+   if(dtMove.collision == false){
+        return dtMove;
+   } else {
+        return this->recursiveMoveCircle(dtMove.pos_final, dtMove.reflex, radius);
+   }
+}
+
+std::vector<std::shared_ptr<Polygon_>> Blocks::selectByRectangle(Vector2 pos, Vector2 size_){
     auto retorno = std::vector<std::shared_ptr<Polygon_>>();
 
     for(std::shared_ptr<Renderizable> i: this->poligonos.elements){
         std::shared_ptr<Polygon_>p = std::dynamic_pointer_cast<Polygon_>(i);
         for (Vector2 point: p->getVertex()){
-            if( collisions::rectangle(point, pos, pos+movement)){
+            if( collisions::rectangle(point, pos, pos+size_)){
                 retorno.push_back(p);
                 p->setColor(red);
                 break;
--- src/Blocks.h
+++ src/Blocks.h
@@ -3,6 +3,19 @@
 
 #include "Conteiner.h"
 #include "geometry/Polygon.h"
+
+typedef struct {
+    Vector2 pos_final;  //Posição final do vetor
+
+    //Vetor que contém o resto do movimento em caso de colisão
+    // ou a direção final do vetor para o proximo frame
+    Vector2 reflex;
+
+    //Indica o ´ponto em que ocorreu a colisão, relativo ao comprimento do vetor,
+    // caso == 1, não houve colisão
+    bool collision;
+} data_moveCircle;
+
 class Blocks
 {
     public:
@@ -11,12 +24,24 @@ class Blocks
         Conteiner poligonos;
 
         // Calcula a posição final do circulo, decrementa os blocos atingidos
-        Vector2 moveCircle(Vector2 pos, Vector2 movement, float radius);
+        data_moveCircle moveCircle(Vector2 pos, Vector2 movement, float radius);
 
         // Desce uma linha de blocos
         void lineDown();
     protected:
-        std::vector<std::shared_ptr<Polygon_>> selectByRectangle(Vector2 pos, Vector2 movement);
+        // calcula as colisões e em que momento isto ocorrou de 0 a 1.
+        data_moveCircle firstCollision(std::vector<std::shared_ptr<Polygon_>> filtered, Vector2 pos, Vector2 mov);
+
+        //Obtem poligonos que têm pelo menos um poonto dentro do retangulo passado como parâmetro
+        std::vector<std::shared_ptr<Polygon_>> selectByRectangle(Vector2 pos, Vector2 size_);
+
+        //Função principal para calcular o movimento do circulo,
+        // a contraparte publica só realiza translação e entra na função recursiva
+        data_moveCircle recursiveMoveCircle(Vector2 pos, Vector2 movement, float radius);
+
+        //Remove blocos com life == 0
+        void sanitizePolygons();
+
 
 };
 
